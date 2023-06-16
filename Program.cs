f86418fc4ccdb1a66b1a39fe97731c8c46738827
Diego <92130153+DiegoPlaninscheck@users.noreply.github.com>
2023-06-16 16:47:58 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,487 @@
+﻿using System;
+
+namespace diegoFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Atividade 01
+
+            //Console.WriteLine("Valor Minimo de peças : ");
+            //int valorMinimo = int.Parse(Console.ReadLine());
+            //Console.WriteLine("valor Maximo de peças: ");
+            //int valorMaximo = int.Parse(Console.ReadLine());
+
+            //decimal estoqueMedio = (valorMinimo + valorMaximo) / 2;
+
+            //Console.WriteLine("O estoque médio do produto é: " + estoqueMedio);
+
+            // Atividade 02
+
+            //Console.WriteLine("Cotação do dolar : ");
+            //decimal cotacaoDolar = decimal.Parse(Console.ReadLine());
+            //Console.WriteLine("Valor em dolares: ");
+            //decimal valorDolares = decimal.Parse(Console.ReadLine());
+
+            //decimal conversao = valorDolares * cotacaoDolar;
+
+            //Console.WriteLine("O valor em reais é: " + conversao);
+
+            // Atividade 03
+
+            //Console.WriteLine("Id do vendedor: ");
+            //int idVendedor = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Id peça: ");
+            //int idPeca= int.Parse(Console.ReadLine());
+            //Console.WriteLine("Preço unitario peca: ");
+            //decimal precoUnitarioPeca = decimal.Parse(Console.ReadLine());
+            //Console.WriteLine("Quantidade vendida: ");
+            //int qtdVendida = int.Parse(Console.ReadLine());
+
+            //decimal valorTotalVendido = precoUnitarioPeca * qtdVendida;
+
+            //decimal comissao = (valorTotalVendido * 5) / 100;
+
+
+            //Console.WriteLine("Vendedor: " + idVendedor);
+            //Console.WriteLine("Peça: " + idPeca);
+            //Console.WriteLine("Quantidade vendida da peça: " + qtdVendida);
+            //Console.WriteLine("Preço unitario da peça: " + precoUnitarioPeca);
+            //Console.WriteLine("Comissão: " + comissao);
+
+            //Atividade 04
+
+            //Console.WriteLine("Digite um numero: ");
+            //int num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite outro numero: ");
+            //int num2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite outro numero: ");
+            //int num3 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite outro numero: ");
+            //int num4 = int.Parse(Console.ReadLine());
+
+            //decimal soma = (num1 + num2) + (num1 + num3) + (num1 + num4) + (num2 + num3) + (num2 + num4) + (num3 + num4);
+
+            //decimal multiplicacao = (num1 * num2) * (num1 + num3) * (num1 * num4) * (num2 * num3) * (num2 * num4) * (num3 * num4);
+
+            //Console.WriteLine("Soma total: " + soma);
+            //Console.WriteLine("Multiplicação total: " + multiplicacao);
+
+            // Atividade 05
+
+            //Console.WriteLine("Tempo de viagem: ");
+            //decimal tempo = decimal.Parse(Console.ReadLine());
+            //Console.WriteLine("Velocidade: ");
+            //decimal velocidade = decimal.Parse(Console.ReadLine());
+
+            //decimal distancia = tempo * velocidade;
+
+            //decimal litros = distancia / 12;
+
+            //Console.WriteLine("Velocidade: " + velocidade);
+            //Console.WriteLine("Tempo: " + tempo);
+            //Console.WriteLine("Distancia: " + distancia);
+            //Console.WriteLine("Litros: " + decimal.Round(litros, 2));
+
+            // Atividade 06
+
+            //Console.WriteLine("Temperatura em graus (C°): ");
+            //decimal celsius = decimal.Parse(Console.ReadLine());
+
+            //decimal fahrenheit = (9 * celsius + 160) / 5;
+
+            //Console.WriteLine("A temperatura em Fahrenheit é: " + fahrenheit);
+
+            // Atividade 07
+
+            //Console.WriteLine("Temperatura em fahrenheit (F°): ");
+            //decimal fahrenheit = decimal.Parse(Console.ReadLine());
+
+            //decimal celsius = (fahrenheit - 32) * 5 / 9;
+
+            //Console.WriteLine("A temperatura em Fahrenheit é: " + celsius);
+
+            // Atividade 08
+
+            //Console.WriteLine("Raio da lata: ");
+            //double raio = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Altura da lata: ");
+            //double altura = double.Parse(Console.ReadLine());
+
+            //double volume = (3.14159 * raio) * raio * altura;
+
+            //Console.WriteLine("Volume é: " + volume);
+
+            // Atividade 09
+
+            //Console.WriteLine("Qual sua idade: ");
+            //int idade = int.Parse(Console.ReadLine());
+
+            //int idadeEmDias = idade * 365;
+
+            //Console.WriteLine("Idade em dias: " + idadeEmDias);
+
+            // Atividade 10
+
+            //Console.WriteLine("Escreva um numero: ");
+            //decimal n1 = decimal.Parse(Console.ReadLine());
+            //Console.WriteLine("Escreva um numero: ");
+            //decimal n2 = decimal.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Igual: " + (n1 == n2));
+            //Console.WriteLine("Não igual: " + (n1 != n2));
+            //Console.WriteLine("Maior: " + (n1 > n2));
+            //Console.WriteLine("Menor: " + (n1 < n2));
+            //Console.WriteLine("Maior igual: " + (n1 >= n2));
+            //Console.WriteLine("Menor igual: " + (n1 <= n2));
+
+            // Atividade 11
+
+            //Console.WriteLine("Escreva um numero: ");
+            //decimal n1 = decimal.Parse(Console.ReadLine());
+            //Console.WriteLine("Escreva um numero: ");
+            //decimal n2 = decimal.Parse(Console.ReadLine());
+
+            //decimal aux = n1;
+
+            //n1 = n2;
+            //n2 = aux;
+
+            //Console.WriteLine(n1);
+            //Console.WriteLine(n2);
+
+
+            // Atividade 12
+
+            //Console.WriteLine("Escreva um numero: ");
+            //decimal n1 = int.Parse(Console.ReadLine());
+
+            //if (n1 < 0)
+            //{
+            //    n1 = n1 * (-1);
+            //}
+
+            //Console.WriteLine("Modulo: " + n1);
+
+
+            // Atividade 13
+
+            //Console.WriteLine("Escreva um numero: ");
+            //decimal n1 = decimal.Parse(Console.ReadLine());
+            //Console.WriteLine("Escreva um numero: ");
+            //decimal n2 = decimal.Parse(Console.ReadLine());
+            //Console.WriteLine("Escreva um numero: ");
+            //decimal n3 = decimal.Parse(Console.ReadLine());
+
+            //decimal maior = 0;
+            //decimal menor = 0;
+            //decimal meio = 0;
+
+            //if (n1 > n2 && n1 > n3)
+            //{
+            //    maior = n1;
+            //}
+            //else if (n2 > n1 && n2 > n3)
+            //{
+            //    maior = n2;
+            //}
+            //else
+            //{
+            //    maior = n3;
+            //}
+
+            //if (n1 < n2 && n1 < n3)
+            //{
+            //    menor = n1;
+            //}
+            //else if (n2 < n1 && n2 < n3)
+            //{
+            //    menor = n2;
+            //}
+            //else
+            //{
+            //    menor = n3;
+            //}
+
+            //if (n1 > n2 && n1 < n3 || n1 < n2 && n1 > n3)
+            //{
+            //    meio = n1;
+            //}
+            //else if (n2 < n1 && n2 > n3 || n2 > n1 && n2 < n3)
+            //{
+            //    meio = n2;
+            //}
+            //else
+            //{
+            //    meio = n3;
+            //}
+
+            //Console.WriteLine(maior + "" + meio + "" + menor);
+
+
+            // Atividade 14
+
+            //Console.WriteLine("Escreva um numero: ");
+            //decimal n1 = decimal.Parse(Console.ReadLine());
+            //Console.WriteLine("Escreva um numero: ");
+            //decimal n2 = decimal.Parse(Console.ReadLine());
+
+            //decimal diferenca = 0;
+
+            //if (n1 > n2)
+            //{
+            //    diferenca = n1 - n2;
+            //} else
+            //{
+            //    diferenca = n2 - n1;
+            //}
+
+            //Console.WriteLine("A diferença é: " + diferenca);
+
+
+            // Atividade 15
+
+            //Console.WriteLine("Escreva um numero: ");
+            //decimal nota1 = decimal.Parse(Console.ReadLine());
+            //Console.WriteLine("Escreva um numero: ");
+            //decimal nota2 = decimal.Parse(Console.ReadLine());
+            //Console.WriteLine("Escreva um numero: ");
+            //decimal nota3 = decimal.Parse(Console.ReadLine());
+            //Console.WriteLine("Escreva um numero: ");
+            //decimal nota4 = decimal.Parse(Console.ReadLine());
+
+            //decimal soma = nota1 + nota2 + nota3 + nota4;
+
+            //decimal media = soma / 4;
+
+            //if (media >= 7)
+            //{
+            //    Console.WriteLine("Aprovado");
+            //} else
+            //{
+            //    Console.WriteLine("Escreva um numero: ");
+            //    decimal nota5 = int.Parse(Console.ReadLine());
+            //    soma = soma + nota5;
+            //    media = soma / 5;
+
+            //    if (media >= 7)
+            //    {
+            //        Console.WriteLine("Aprovado");
+            //    } else
+            //    {
+            //        Console.WriteLine("Reprovado com média: " + media);
+            //    }
+            //}
+
+
+            // Atividade 16
+
+            //Console.WriteLine("Escreva um numero: ");
+            //decimal num1 = decimal.Parse(Console.ReadLine());
+            //Console.WriteLine("Escreva um numero: ");
+            //decimal num2 = decimal.Parse(Console.ReadLine());
+
+            //if (num1 > num2)
+            //{
+            //    Console.WriteLine("O maior numero é:" + num1);
+            //    Console.WriteLine("O menor numero é:" + num2);
+            //}
+            //else if (num2 > num1)
+            //{
+            //    Console.WriteLine("O maior numero é:" + num2);
+            //    Console.WriteLine("O menor numero é:" + num1);
+            //}
+            //else if (num1 == num2)
+            //{
+            //    Console.WriteLine("Os numeros são iguais: " + num1 + "" + num2);
+            //}
+
+
+            // Atividade 17
+
+            //Console.WriteLine("Escreva um numero: ");
+            //decimal num = decimal.Parse(Console.ReadLine());
+
+            //if (num >= 0 && num <= 9)
+            //{
+            //    Console.WriteLine("Valor valido:" + num);
+            //}else
+            //{
+            //    Console.WriteLine("Valor invalido:" + num);
+            //}
+
+
+            // Atividade 18
+
+            //Console.WriteLine("Codigo: ");
+            //int codigo = int.Parse(Console.ReadLine());
+
+            //switch (codigo)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Um");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Dois");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Tres");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Codigo invalido");
+            //        break;
+            //}
+
+
+            // Atividade 19
+
+            //Console.WriteLine("Codigo: ");
+            //int codigo = int.Parse(Console.ReadLine());
+
+
+            // Atividade 26
+
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            // Atividade 27
+
+            //Console.WriteLine("Digite um numero: ");
+            //int numero = int.Parse(Console.ReadLine());
+
+            //int fatorial = 1;
+
+            //for (int i = 1; i <= numero; i++)
+            //{
+            //    fatorial = fatorial * i;
+            //    Console.WriteLine("Fatorial do numero: " + i + " = " + fatorial);
+            //}
+
+
+            // Atividade 28
+
+            //for (int i = 100; i <= 200; i++)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+
+            // Atividade 29
+
+            //for (int i = 1; i <= 2000; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            // Atividade 30
+
+            //Console.WriteLine("Digite um numero: ");
+            //int numero = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i<= 10; i++)
+            //{
+            //    Console.WriteLine(numero + " X " + i + " = " + (i * numero));
+            //}
+
+
+            // Atividade 31
+
+            //Console.WriteLine("Digite um numero: ");
+            //int numero = int.Parse(Console.ReadLine());
+
+            //int somaPar = 0, somaImpar = 0; ;
+
+            //for (int i = numero; i > 0; i--)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine("É par: " + i);
+            //        somaPar += i;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("É impar: " + i);
+            //        somaImpar += i;
+            //    }
+            //}
+
+            //Console.WriteLine("Soma dos pares: " + somaPar);
+            //Console.WriteLine("Soma dos impares: " + somaImpar);
+
+
+            // Atividade 32
+
+            //Console.WriteLine("Digite um numero: (1 - 9)");
+            //int numero = int.Parse(Console.ReadLine());
+
+            //double soma = 0;
+
+            //for (int i = numero; i <= 20; i++)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        Console.WriteLine(i + " - " + soma);
+            //        soma += (Math.Pow(i, 2));
+            //    }
+            //}
+
+            //Console.WriteLine("A soma dos numeros ao quadrado impares do 20 primeiros numero é: " + Math.Round(soma, 2));
+
+
+            // Atividade 33
+
+            //Console.WriteLine("Digite a quantidade de alunos: ");
+            //int numero = int.Parse(Console.ReadLine());
+
+            //decimal soma = 0;
+
+            //for (int i = 1; i <= numero; i++)
+            //{
+            //    Console.WriteLine("Digite a nota: ");
+            //    decimal nota = decimal.Parse(Console.ReadLine());
+
+            //    soma += nota;
+            //}
+
+            //decimal media = soma / numero;
+
+            //Console.WriteLine("A media é: " + media);
+
+
+            // Atividade 45
+
+            //Console.WriteLine("Qual o tamanho do vetor: ");
+            //int tamanhoVetor = int.Parse(Console.ReadLine());
+
+            //decimal[] vetor = new decimal[tamanhoVetor];
+
+            //for (int i = 0; i < tamanhoVetor; i++)
+            //{
+            //    Console.WriteLine("Valor: ");
+            //    decimal numero = decimal.Parse(Console.ReadLine());
+            //    vetor[i] = numero;
+            //}
+
+            //for (int i = tamanhoVetor - 1; i >= 0; i--)
+            //{
+            //    Console.WriteLine("\n" + vetor[i]);
+            //}
+
+
+            // Atividade 46
+
+
+
+        }
+    }
+
+}
