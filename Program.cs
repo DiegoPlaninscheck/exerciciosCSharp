a509df674bd1b11413656b1ccb618a98d7c7ebae
Diego <92130153+DiegoPlaninscheck@users.noreply.github.com>
2023-06-18 21:49:59 -0300

    feat: mais questoes feitas

--- Program.cs
+++ Program.cs
@@ -756,9 +756,180 @@ namespace diegoFirstApp
             //}
 
 
-            // Atividade 55
+            // Atividade 62
 
-            
+            //Console.WriteLine("Qual é a altura do triangulo: ");
+            //decimal alturaTriangulo = decimal.Parse(Console.ReadLine());
+            //Console.WriteLine("Qual é a base do triangulo: ");
+            //decimal baseTriangulo = decimal.Parse(Console.ReadLine());
+
+            //hipotenusa(baseTriangulo, alturaTriangulo);
+
+            //void hipotenusa(decimal baseTriangulo, decimal alturaTriangulo)
+            //{
+            //    decimal hipotenusa = (baseTriangulo * baseTriangulo) + (alturaTriangulo * alturaTriangulo);
+            //    Console.WriteLine("A hipotenusa é: " + hipotenusa);
+            //}
+
+
+            // Atividade 63
+
+            //Console.WriteLine("Escreva um numero qualquer: ");
+            //int numero = int.Parse(Console.ReadLine());
+
+            //verificaImparPar(numero);
+
+            //void verificaImparPar(int numero)
+            //{
+            //    if (numero % 2 == 0)
+            //    {
+            //        Console.WriteLine("É par");
+            //    }else
+            //    {
+            //        Console.WriteLine("É impar");
+            //    }
+            //}
+
+
+            // Atividade 64
+
+            int[] numeros;
+
+            Console.WriteLine("Qual o tamanho do array: ");
+            int tamanhoArray = int.Parse(Console.ReadLine());
+
+            numeros = new int[tamanhoArray];
+
+            while (true)
+            {
+
+                Console.WriteLine("Escolhar uma opção abaixo: \n1- Carregar Array" +
+                    "\n2- Listar Array" +
+                    "\n3- Exibir apenas os números pares do vetor" +
+                    "\n4- Exibir apenas os números ímpares do vetor" +
+                    "\n5- Exibir a quantidade de números pares existem nas posições ímpares do vetor" +
+                    "\n6- Exibir a quantidade de números ímpares existem nas posições pares do vetor" +
+                    "\n7- Sair");
+                int opcao = int.Parse(Console.ReadLine());
+
+                menu(opcao);
+
+                void menu(int opcao)
+                {
+                    switch (opcao)
+                    {
+                        case 1:
+                            {
+                                Console.WriteLine("Carregar Array");
+                                carregarArray(numeros);
+                                break;
+                            }
+                        case 2:
+                            {
+                                Console.WriteLine("Listar Vetor");
+                                listarArray(numeros);
+                                break;
+                            }
+                        case 3:
+                            {
+                                Console.WriteLine("Exibir apenas os números pares do vetor");
+                                numerosParesArray(numeros);
+                                break;
+                            }
+                        case 4:
+                            {
+                                Console.WriteLine("Exibir apenas os números ímpares do vetor");
+                                numerosImparesArray(numeros);
+                                break;
+                            }
+                        case 5:
+                            {
+                                Console.WriteLine("Exibir a quantidade de números pares existem nas posições ímpares do vetor");
+                                numerosParesPosicaoImpar(numeros);
+                                break;
+                            }
+                        case 6:
+                            {
+                                Console.WriteLine("Exibir a quantidade de números ímpares existem nas posições pares do vetor");
+                                numerosImparesPosicaoPar(numeros);
+                                break;
+                            }
+                        case 7:
+                            {
+                                Console.WriteLine("Acabou...");
+                                break;
+                            }
+                    }
+                }
+            }
+
+            void carregarArray(int[] numeros)
+            {
+                Console.WriteLine("Digite um numero: ");
+                int numero = int.Parse(Console.ReadLine());
+
+                numeros.Append(numero);
+            }
+
+            void listarArray(int[] numeros)
+            {
+                foreach (int numero in numeros)
+                {
+                    Console.WriteLine(numero);
+                }
+            }
+
+            void numerosParesArray(int[] numeros)
+            {
+                foreach (int numero in numeros)
+                {
+                    if (numero % 2 == 0)
+                    {
+                        Console.WriteLine(numero);
+                    }
+                }
+            }
+
+            void numerosImparesArray(int[] numeros)
+            {
+                foreach (int numero in numeros)
+                {
+                    if (numero % 2 != 0)
+                    {
+                        Console.WriteLine(numero);
+                    }
+                }
+            }
+
+            void numerosParesPosicaoImpar(int[] numeros)
+            {
+                int index = 0;
+
+                foreach (int numero in numeros)
+                {
+                    if (numero % 2 == 0 && numeros[index] % 2 != 0)
+                    {
+                        Console.WriteLine(numero);
+                    }
+
+                    index++;
+                }
+            }
+
+            void numerosImparesPosicaoPar(int[] numeros)
+            {
+                int index = 0;
+
+                foreach (int numero in numeros)
+                {
+                    if (numero % 2 != 0 && numeros[index] % 2 == 0)
+                    {
+                        Console.WriteLine(numero);
+                    }
+
+                    index++;
+                }
+            }
         }
     }
 }
